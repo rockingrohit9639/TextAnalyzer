0bf492026a3f3c51f6429ef6c2df8550e8eed7bf
ShudhanshuSingh <shudhanshusingh051@gmail.com>
2021-02-17 13:27:39 +0530

    UI improvement

--- mysite/views.py
+++ mysite/views.py
@@ -2,19 +2,44 @@ from django.http import HttpResponse
 from django.shortcuts import render
 import string
 
+
 def index(request):
 
     return render(request, 'index.html')
 
+
 def analyze(request):
+
     puncts = string.punctuation
+    word_to_find = request.POST.get("word_input")
     djText = request.POST.get('text', 'default')
     remPunc = request.POST.get('removepunc', 'off')
     cap = request.POST.get('capitalize', 'off')
     small = request.POST.get('toSmall', 'off')
     upper = request.POST.get('toUpper', 'off')
+    word_find_flag = request.POST.get('word_find', 'off')
     analyzed_text = ""
-    if remPunc == "on" and cap == "on":
+    word_status = ""
+
+    countword = len(djText.split())
+
+    if word_find_flag == "on":
+        if word_to_find != "":
+            if djText.find(word_to_find) != -1:
+                word_status = "found"
+            else:
+                word_status = "not found"
+            analyzed_text = djText
+
+            result = {
+                "analyzed_text": analyzed_text,
+                "purpose": "Find",
+                "status": word_status,
+                "wordcount": countword
+            }
+
+    elif remPunc == "on" and cap == "on":
+
         for char in djText:
             if char not in puncts:
                 analyzed_text = analyzed_text + char
@@ -22,7 +47,8 @@ def analyze(request):
         analyzed_text = analyzed_text.capitalize()
         result = {
             "analyzed_text": analyzed_text,
-            "purpose": "Remove Punctuations & Capitalize"
+            "purpose": "Remove Punctuations & Capitalize",
+            "wordcount": countword
         }
     elif remPunc == "on" and small == "on":
         for char in djText:
@@ -32,7 +58,8 @@ def analyze(request):
         analyzed_text = analyzed_text.lower()
         result = {
             "analyzed_text": analyzed_text,
-            "purpose": "Remove Punctuations & To Small"
+            "purpose": "Remove Punctuations & To Small",
+            "wordcount": countword
         }
 
     elif remPunc == "on" and upper == "on":
@@ -43,7 +70,8 @@ def analyze(request):
         analyzed_text = analyzed_text.upper()
         result = {
             "analyzed_text": analyzed_text,
-            "purpose": "Remove Punctuations & To Upper"
+            "purpose": "Remove Punctuations & To Upper",
+            "wordcount": countword
         }
 
     elif cap == "on" and small == "on":
@@ -52,7 +80,8 @@ def analyze(request):
         analyzed_text = analyzed_text.lower()
         result = {
             "analyzed_text": analyzed_text,
-            "purpose": "Capitalize & To Small"
+            "purpose": "Capitalize & To Small",
+            "wordcount": countword
         }
 
     elif cap == "on" and upper == "on":
@@ -61,13 +90,15 @@ def analyze(request):
         analyzed_text = analyzed_text.upper()
         result = {
             "analyzed_text": analyzed_text,
-            "purpose": "Capitalize & To Upper"
+            "purpose": "Capitalize & To Upper",
+            "wordcount": countword
         }
     elif small == "on" and upper == "on":
         analyzed_text = "Text can be smaller or uppercase only."
         result = {
             "analyzed_text": analyzed_text,
-            "purpose": "Small & Upper"
+            "purpose": "Small & Upper",
+            "wordcount": countword
         }
 
     elif remPunc == 'on':
@@ -76,14 +107,16 @@ def analyze(request):
                 analyzed_text = analyzed_text + char
         result = {
             "analyzed_text": analyzed_text,
-            "purpose": "Remove Punctuations"
+            "purpose": "Remove Punctuations",
+            "wordcount": countword
         }
     elif cap == "on":
         analyzed_text = djText.capitalize()
 
         result = {
             "analyzed_text": analyzed_text,
-            "purpose": "Capitalize"
+            "purpose": "Capitalize",
+            "wordcount": countword
         }
 
     elif small == "on":
@@ -91,7 +124,8 @@ def analyze(request):
 
         result = {
             "analyzed_text": analyzed_text,
-            "purpose": "To Smallercase"
+            "purpose": "To Smallercase",
+            "wordcount": countword
         }
 
     elif upper == "on":
@@ -99,7 +133,8 @@ def analyze(request):
 
         result = {
             "analyzed_text": analyzed_text,
-            "purpose": "To Uppercase"
+            "purpose": "To Uppercase",
+            "wordcount": countword
         }
     else:
         return HttpResponse('''<script type="text/javascript">alert("Please select atleast one option.");</script>''')
