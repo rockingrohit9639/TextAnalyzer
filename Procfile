707eede9dc703fc1d130d6f400254ce82e9fb680
Rohit Kumar Saini <rohitbhatwara1@gmail.com>
2021-02-24 10:43:04 +0530

    deploy fix

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
-web: gunicorn mysite.wsgi --log-file -
+web: gunicorn mysite.wsgi:application --log-file - --log-level debug
+python manage.py collectstatic --noinput
+manage.py migrate
--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,8 +1,3 @@
-"""
-WSGI config for mysite project.
-It exposes the WSGI callable as a module-level variable named ``application``.
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
