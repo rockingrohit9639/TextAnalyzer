ec3faf044ebf68e5420e8236f85f18291e38ff0c
arbazalam <drarbazalam@gmail.com>
2021-03-02 13:24:37 +0530

    Updated copy feature

--- static/js/Script.js
+++ static/js/Script.js
@@ -1,15 +1,26 @@
-const analyzedtext = document.getElementById("analyzed_text");
 
+function copyToClipboard(elementId) {
 
-
-function myFunction() {
-    analyzedtext.focus();
-    analyzedtext.select();
+    // Create an auxiliary hidden input
+    var aux = document.createElement("input");
+  
+    // Get the text from the element passed into the input
+    aux.setAttribute("value", document.getElementById(elementId).innerHTML);
+  
+    // Append the aux input to the body
+    document.body.appendChild(aux);
+  
+    // Highlight the content
+    aux.select();
+  
+    // Execute the copy command
     document.execCommand("copy");
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-    // setTimeout(function () {
-    //     popup.classList.remove("show");
-    // }, 1000);
-}
-// console.log("HEllo");
+  
+    // Remove the input from the body
+    document.body.removeChild(aux);
+  
+  }
+  
+  function log(){
+      console.log('---')
+  }
