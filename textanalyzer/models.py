6cccf7d046354799ab65a5d3b2b0d7fe48d47cbf
Rohit Kumar Saini <40729749+rockingrohit9639@users.noreply.github.com>
2021-03-22 19:28:24 +0530

    Update textanalyzer/models.py
    Co-authored-by: django-doctor[bot] <72320148+django-doctor[bot]@users.noreply.github.com>

--- textanalyzer/models.py
+++ textanalyzer/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
 class User_profile(models.Model):
-    name=models.CharField(max_length=200,null=True)
+    name = models.CharField(max_length=200, default="", blank=True)
     email=models.EmailField(max_length=100,null=True)
 
     def __str__(self):
         return str(self.name)
-
 
